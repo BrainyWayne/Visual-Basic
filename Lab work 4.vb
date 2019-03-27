de4b1808bb634ff6eda734c7bde37c72af25971a
Francis Eshun <38195639+BrainyWayne@users.noreply.github.com>
2019-03-27 19:15:51 +0000

    Update Lab work 4.vb

--- Lab work 4.vb
+++ Lab work 4.vb
@@ -1,168 +1,220 @@
-﻿Public Class Form1
+﻿
+'LAB4 WITH FUNCTIONS
 
-	'Lab work 4
-	
-	'Declaring global variables
-    Private valid As Boolean = False
-    Private PricePerPart As Double
-    Private Quantity As Double
+Public Class Form1
+
     Private Cost As Double
-    Private SalesTax As Double = 0
-    Private TaxHandling As Double = 0
-    Private Handlingcharge As Double = 0
-
-    Private Sub Label6_Click(sender As Object, e As EventArgs) Handles Label6.Click
+    Private Salestax As Double
+    Private Shippingfee As Double
+    Private Handlingfee As Double
+    Private valid = True
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
 
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-	
-	'These instructions are run right after the form is loaded
-        txtName.Focus()  'set the cursor focus to the Customer name textbox
-        chkboxWholesale.Checked = False  'uncheck the wholesale Checkbox
-        txtState.CharacterCasing = CharacterCasing.Upper   'Set the text in the State check box to upper case
-        radioUspostal.Checked = True 'check the US postal radio button
+        txtName.Focus()
+        chkboxWholesale.Checked = "False"
+        txtstate.CharacterCasing = CharacterCasing.Upper
+        rduspostal.Checked = True
     End Sub
 
-    Private Sub btnCompute_Click(sender As Object, e As EventArgs) Handles btnCompute.Click
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles Customer_Information.Enter
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Computebtn.Click
         Dim total As Double
-        ValidData()  'Call the ValidData function
+        ValidData()
+        Cost = (txtPPP.Text * txtquantity.Text).ToString("n1")
+        ComputeSalesTaxDue()
+        ComputeTransportHandling()
 
-        Cost = (txtPriceperpart.Text * txtQuantity.Text).ToString("n1")
-        ComputeSalesTaxDue() 'Call the ComputeSalesTaxDue function
-        ComputeTransportaionHandling() 'Call the ComputeTransportaionHandling function
+        total = Salestax + Handlingfee + Shippingfee
 
-
-        total = Cost + ComputeSalesTaxDue() + ComputeTransportaionHandling()
-
-        txtCost.Text = Cost
-        txtTotal.Text = total.ToString("n1")
-        txtTaxDue.Text = ComputeSalesTaxDue()
-        txtShipping.Text = ComputeTransportaionHandling()
+        txtcost.Text = Cost
+        txttaxdue.Text = ComputeSalesTaxDue()
+        txtshipnhand.Text = ComputeTransportHandling()
+        txttotal.Text = total.ToString("N1")
     End Sub
 
-	'ComputeTransportaionHandling function
-    Private Function ComputeTransportaionHandling()
-        If (radioUspostal.Checked = True) Then
-            TaxHandling = 0.15
+
+    Private Function ComputeTransportHandling()
+        If (rduspostal.Checked = True) Then
+            Shippingfee = 0.15
         End If
 
-        If (radioUspostalsir.Checked = True) Then
-            TaxHandling = 0.5
+        If (rduspostalA.Checked = True) Then
+            Shippingfee = 0.5
         End If
 
-        If (txtState.Text = "MO" Or txtState.Text = "IL" Or txtState.Text = "KY") Then
-            Handlingcharge = 0
+        If (txtstate.Text = "MO" Or txtstate.Text = "IL" Or txtstate.Text = "KY") Then
+            Handlingfee = 0
         Else
-            Handlingcharge = 5.0
+            Handlingfee = 5
         End If
-
-        Return Handlingcharge + TaxHandling
+        Return Handlingfee + Shippingfee
     End Function
-
-	
-	'ComputeSalesTaxDue function
     Private Function ComputeSalesTaxDue()
-        If (txtState.Text = "IL") Then
-            SalesTax = 0.07
+
+        If (txtstate.Text = "IL") Then
+            Salestax = 0.07
         End If
 
-        If (txtState.Text = "NY" Or txtState.Text = "CA") Then
-            SalesTax = 0.08
+        If (txtstate.Text = "NY" Or txtstate.Text = "CA") Then
+            Salestax = 0.08
         End If
-
         If (chkboxWholesale.Checked = True) Then
-            SalesTax = 0
+            Salestax = 0
         End If
-
-        Return SalesTax
+        Return Salestax
     End Function
 
-	
-	'ValidData function
     Private Function ValidData()
-
-        If (txtName.Text = String.Empty) Then
-            MessageBox.Show("The Customer name cannot be empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        If (txtName.Text = "Null") Then
+            MessageBox.Show("The Customer name can't be Empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             txtName.Focus()
 
-        ElseIf (txtAddress.Text = String.Empty) Then
-            MessageBox.Show("The Address cannot be empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        ElseIf (txtAddress.Text = "Null")
+            MessageBox.Show("The Customer Address can't be Empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             txtAddress.Focus()
-
-        ElseIf (txtCity.Text = String.Empty) Then
-            MessageBox.Show("The City cannot be empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+        If (txtCity.Text = "Null") Then
+            MessageBox.Show("The Customer city can't be Empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             txtCity.Focus()
 
-        ElseIf (txtState.Text = String.Empty Or txtState.Text.Trim.Length <> 2) Then
-            MessageBox.Show("The State cannot be empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            txtState.Focus()
+        ElseIf (txtstate.Text = "Null")
+            MessageBox.Show("The Customer name can't be Empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtstate.Focus()
+        End If
+        If (txtZipCode.Text = "Null" Or txtZipCode.Text.Trim.Length < 5) Then
+            MessageBox.Show("The ZipCode can't be Empty Or Less Than ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtZipCode.Focus()
 
-        ElseIf (txtZip.Text = String.Empty Or txtZip.Text.Trim.Length < 5) Then
-            MessageBox.Show("The Zip Code cannot be empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            txtZip.Focus()
-
-        ElseIf (txtDescription.Text = String.Empty) Then
-            MessageBox.Show("The Description cannot be empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        ElseIf (txtDescription.Text = "Null")
+            MessageBox.Show("The Description can't be Empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
             txtDescription.Focus()
 
-        ElseIf (txtPriceperpart.Text <= 0) Then
+        End If
+        If (txtPPP.Text <= 0) Then
             Try
+                MessageBox.Show("The Price Per Part must be greater than zero", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                txtPPP.Focus()
+                txtPPP.SelectAll()
+            Catch ex As Exception
+            End Try
+        ElseIf (IsNumeric(txtPPP.Text) = False) Then
+            ValidData = False
+            MessageBox.Show("The Price Per Part must be numeric", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtPPP.Focus()
+            txtPPP.SelectAll()
 
-                MessageBox.Show("The Price per part must be greater than 0", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                txtPriceperpart.Focus()
-                txtPriceperpart.SelectAll()
+
+        ElseIf (Double.Parse(txtWPP.Text) <= 0) Then
+            Try
+                ValidData = False
+                MessageBox.Show("The Weight Per Part must be greater zero", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                txtWPP.Focus()
+                txtWPP.SelectAll()
+            Catch ex As Exception
+            End Try
+
+        ElseIf (IsNumeric(txtWPP.Text) = False) Then
+            ValidData = False
+            MessageBox.Show("The Weight Per Part must be numeric", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtWPP.Focus()
+            txtWPP.SelectAll()
+
+
+        ElseIf (Double.Parse(txtquantity.Text) <= 0)
+
+            Try
+                ValidData = False
+                MessageBox.Show("The quantity can't be Empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                txtquantity.Focus()
+                txtquantity.SelectAll()
             Catch ex As Exception
 
             End Try
 
-        ElseIf (IsNumeric(txtPriceperpart.Text) = False) Then
-            valid = False
-            MessageBox.Show("The Price per part must be a number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            txtPriceperpart.Focus()
-            txtPriceperpart.SelectAll()
+        ElseIf (IsNumeric(txtquantity.Text) = False) Then
 
-        ElseIf (Double.Parse(txtWeightperpart.Text) <= 0) Then
-            Try
-
-                valid = False
-                MessageBox.Show("The Weight per part must be greater than 0", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                txtWeightperpart.Focus()
-                txtWeightperpart.SelectAll()
-            Catch ex As Exception
-
-            End Try
-
-
-        ElseIf (IsNumeric(txtWeightperpart.Text) = False) Then
-            valid = False
-            MessageBox.Show("The Weight per part must be a number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            txtWeightperpart.Focus()
-            txtWeightperpart.SelectAll()
-
-        ElseIf (Double.Parse(txtQuantity.Text) <= 0) Then
-            Try
-
-                valid = False
-                MessageBox.Show("The Quantity must be greater than 0", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-                txtQuantity.Focus()
-                txtQuantity.SelectAll()
-            Catch ex As Exception
-
-            End Try
-
-
-        ElseIf (IsNumeric(txtQuantity.Text) = False) Then
-
-            MessageBox.Show("The Quantity must be a number", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-            txtQuantity.Focus()
-            txtQuantity.SelectAll()
-
-
+            MessageBox.Show("The Weight Per Part must be numeric", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            txtquantity.Focus()
+            txtquantity.SelectAll()
         Else
-            valid = True
+            ValidData = True
+
         End If
 
-
     End Function
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub TextBox10_TextChanged(sender As Object, e As EventArgs) Handles txtcost.TextChanged
+
+    End Sub
+
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+
+    End Sub
+    Private Sub NewOrderbtn_Click(sender As Object, e As EventArgs) Handles NewOrderbtn.Click
+        txttotal.Clear()
+        txtstate.Clear()
+        txtPPP.Clear()
+        txtWPP.Clear()
+        txtName.Clear()
+        txtDescription.Clear()
+        txtstate.Clear()
+        txtcost.Clear()
+        txtshipnhand.Clear()
+        txtCity.Clear()
+        txtZipCode.Clear()
+        txtquantity.Clear()
+        txttotal.Clear()
+        chkboxWholesale.Checked = False
+        rduspostal.Checked = False
+        rduspostalA.Checked = False
+        txtName.Focus()
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Exitbtn.Click
+        Me.Close()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtName.TextChanged
+
+    End Sub
+
+    Private Sub chkboxWholesale_CheckedChanged(sender As Object, e As EventArgs) Handles chkboxWholesale.CheckedChanged
+
+    End Sub
+
+    Private Sub txtstate_TextChanged(sender As Object, e As EventArgs) Handles txtstate.TextChanged
+
+    End Sub
+
+    Private Sub txtPPP_TextChanged(sender As Object, e As EventArgs) Handles txtPPP.TextChanged
+
+    End Sub
+
+    Private Sub txtZipCode_TextChanged(sender As Object, e As EventArgs) Handles txtZipCode.TextChanged
+
+    End Sub
+
+    Private Sub txtAddress_TextChanged(sender As Object, e As EventArgs) Handles txtAddress.TextChanged
+
+    End Sub
+
+    Private Sub txttaxdue_TextChanged(sender As Object, e As EventArgs) Handles txttaxdue.TextChanged
+
+    End Sub
 End Class
